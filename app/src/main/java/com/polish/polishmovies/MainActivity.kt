8a14e7f1b87a519536e37296b786796e2c8f27f6
Osehi <osehiaseehilen@gmail.com>
2020-02-09 05:14:54 +0100

    'tablayout'

--- app/src/main/java/com/polish/polishmovies/MainActivity.kt
+++ app/src/main/java/com/polish/polishmovies/MainActivity.kt
@@ -2,11 +2,24 @@ package com.polish.polishmovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.polish.polishmovies.adapter.DPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // setup the tool bar and toolBar title.
+        toolBarId.setTitle("Movies")
+        setSupportActionBar(toolBarId)
+
+        // setup the adapter
+        val fragAdapter = DPagerAdapter(supportFragmentManager)
+        viewPagerId.adapter = fragAdapter
+
+        // connect the tablayou to the viewPager
+        tablayoutId.setupWithViewPager(viewPagerId)
     }
 }
