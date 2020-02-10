c77dc229f8fa0f4a53e06c487ba6bc821ebfeaae
Osehi <osehiaseehilen@gmail.com>
2020-02-10 11:39:33 +0100

    'new-Activities'

--- app/src/main/java/com/polish/polishmovies/activity/FavouriteMovies.kt
+++ app/src/main/java/com/polish/polishmovies/activity/FavouriteMovies.kt
@@ -6,21 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 
 import com.polish.polishmovies.R
+import kotlinx.android.synthetic.main.fragment_favourite_movies.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class FavouriteMovies : Fragment() {
 
+    lateinit var favorite:Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite_movies, container, false)
+        val view = inflater.inflate(R.layout.fragment_favourite_movies, container, false)
+
+        favorite = view.findViewById(R.id.buttonFavoriteId)
+
+        favorite.setOnClickListener {
+            view.findNavController().navigate(R.id.action_favouriteMovies_to_movieDetails)
+        }
+
+        return view
+
     }
+
 
 
 }
--- app/src/main/java/com/polish/polishmovies/activity/MovieDetails.kt
+++ app/src/main/java/com/polish/polishmovies/activity/MovieDetails.kt
@@ -6,26 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
 
 import com.polish.polishmovies.R
-import kotlinx.android.synthetic.main.fragment_all_movies.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class AllMovies : Fragment() {
+class MovieDetails : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_all_movies, container, false)
+        val view = inflater.inflate(R.layout.fragment_movie_details, container, false)
 
-//        buttonAllMoviesId.setOnClickListener {
-//            view.findNavController().navigate(R.id.action_allMovies2_to_movieDetails)
-//        }
 
 
         return view
